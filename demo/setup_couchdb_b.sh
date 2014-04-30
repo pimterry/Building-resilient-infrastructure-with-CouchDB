8105aa18ed3e0a4da0f2311726144de2b29989d6
Tim Perry <pimterry@gmail.com>
2014-04-30 14:48:13 +0100

    Write public files to gh-pages

--- demo/setup_couchdb_b.sh
+++
@@ -1,6 +0,0 @@
-curl -X PUT http://localhost:5984/_replicator/a_to_b \
-     -H 'Content-Type: application/json' \
-     -d '{ "_id": "a_to_b",
-           "target": "demo-db",
-           "source": "http://10.0.100.10:5984/demo-db/",
-           "continuous": true }'
