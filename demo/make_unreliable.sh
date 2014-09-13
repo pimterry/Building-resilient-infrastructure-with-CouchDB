0140096dcc62653c2ce7d00018b338a835eda841
pimterry <pimterry@gmail.com>
2014-09-13 14:06:19 +0100

    Add some spacing to improve log output from couchdb killer

--- demo/make_unreliable.sh
+++ demo/make_unreliable.sh
@@ -1,22 +1,38 @@
 while true
 do
+  echo
   echo `date +"%T"` Stopping CouchDB A
+  echo
   vagrant halt couchdb-a --force
+  echo
   echo `date +"%T"` CouchDB A stopped
+  echo
   sleep 30
 
+  echo
   echo `date +"%T"` Restarting CouchDB A
+  echo
   vagrant up couchdb-a --no-provision
+  echo
   echo `date +"%T"` CouchDB A restarted
+  echo
   sleep 30
 
+  echo
   echo `date +"%T"` Stopping CouchDB B
+  echo
   vagrant halt couchdb-b --force
+  echo
   echo `date +"%T"` CouchDB B stopped
+  echo
   sleep 30
 
+  echo
   echo `date +"%T"` Restarting CouchDB B
+  echo
   vagrant up couchdb-b --no-provision
+  echo
   echo `date +"%T"` CouchDB B restarted
+  echo
   sleep 30
 done
