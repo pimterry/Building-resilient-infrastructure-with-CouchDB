1efdc9c012a4e45da9cf818770de21a1d04665bf
Tim Perry <pimterry@gmail.com>
2014-09-14 23:45:13 +0100

    Tweak demo script, to put nano in docs and clarify breakage name

--- demo/create_docs.sh
+++ demo/create_docs.sh
@@ -2,11 +2,11 @@ while true
 do
   curl -X POST 'http://couchdb-A:5984/demo-db' \
        -H "content-type: application/json" \
-       -d '{ "created_at": "'"`date`"'" }' \
+       -d '{ "created_at": "'"`date +%s%N`"'" }' \
        --max-time 0.1
 
   curl -X POST 'http://couchdb-B:5984/demo-db' \
        -H "content-type: application/json" \
-       -d '{ "created_at": "'"`date`"'" }' \
+       -d '{ "created_at": "'"`date +%s%N`"'" }' \
        --max-time 0.1
 done
